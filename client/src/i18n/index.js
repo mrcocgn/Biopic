64e15c4300cac9ba580643eb61700e3cb91f0681
Marcos Ilgi <marcos.ilgi@gmail.com>
2025-03-22 17:00:59 +0100

    Update title in index.html and add language detection support for i18next

--- client/src/i18n/index.js
+++ client/src/i18n/index.js
@@ -1,6 +1,8 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
+// Sprachdateien
 import en from './locales/en.json';
 import de from './locales/de.json';
 import tr from './locales/tr.json';
@@ -8,6 +10,7 @@ import es from './locales/es.json';
 import ar from './locales/ar.json';
 
 i18n
+  .use(LanguageDetector) // <--- Sprache erkennen
   .use(initReactI18next)
   .init({
     resources: {
@@ -15,13 +18,17 @@ i18n
       de: { translation: de },
       tr: { translation: tr },
       es: { translation: es },
-      ar: { translation: ar },
+      ar: { translation: ar }
     },
-    lng: 'en', // Standardsprache
-    fallbackLng: 'en',
+    fallbackLng: 'en', // Falls Sprache nicht unterstützt wird
     interpolation: {
-      escapeValue: false,
+      escapeValue: false
     },
+    detection: {
+      order: ['localStorage', 'navigator'], // zuerst gespeicherte Sprache, dann Browsersprache
+      caches: ['localStorage']
+    }
   });
 
 export default i18n;
+
